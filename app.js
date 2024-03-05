9c5f2b58933e330d7e86e6520153443a9d928bea
Osama <Osamafazal439@gmail.com>
2024-03-05 14:16:49 +0500

    JavaScript

---
+++ app.js
@@ -0,0 +1,8 @@
+document.write("<h1>Hello <span class='special'>JavaScript</span> Planet</h1>"
+)
+document.write("<p> I am the <b>master</b> of my own fate."
+)
+document.write("<p> I am the <b>champion</b> of my own soule."
+)
+document.write("<p> I am the <b>superhero</b> of my own life."
+)
